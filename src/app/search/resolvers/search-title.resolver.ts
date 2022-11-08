6919cae73822bd37cd8722fb2d9fb28eb90a4e2f
olcay <olcayusta02@gmail.com>
2022-11-08 19:59:36 +0300

    new fixes

--- src/app/search/resolvers/search-title.resolver.ts
+++ src/app/search/resolvers/search-title.resolver.ts
@@ -1,13 +1,7 @@
-import { Injectable } from '@angular/core';
-import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ResolveFn } from '@angular/router';
+import { of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class SearchTitleResolver implements Resolve<string> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<string> {
-    const { q } = route.queryParams;
-    return of(q);
-  }
-}
+export const searchTitleResolveFn: ResolveFn<string> = (route, state) => {
+  const { q } = route.queryParams;
+  return of(q);
+};
--- src/app/search/search.routes.ts
+++ src/app/search/search.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search.component';
-import { SearchTitleResolver } from './resolvers/search-title.resolver';
+import { searchTitleResolveFn } from './resolvers/search-title.resolver';
 
 export default [
   {
     path: '',
     component: SearchComponent,
-    title: SearchTitleResolver
+    title: searchTitleResolveFn
   }
 ] as Routes;
