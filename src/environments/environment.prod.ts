db620315477d25b2764c38dd9dd8c35e09013d0f
olcay <olcayusta02@gmail.com>
2023-01-02 20:33:10 +0300

    update project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,3 +9,5 @@ export const environment = {
 };
 
 export const API_URL = 'http://localhost:9001'
+export const PUBLIC_VAPID_KEY_OF_SERVER = 'BBuui5T3pIWV_ZyyZel8ynOQLlJO5juM6knPoBzg9shZjkmcqaFm0okyCbWOw2pxSSdEo2p9kiSQYn0V8A-GOjE';
+
